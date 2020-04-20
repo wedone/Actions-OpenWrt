65029c2fd91ccc005c0df074268f0488207959bf
wedone <wedone@users.noreply.github.com>
2020-04-21 01:39:01 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -10,6 +10,6 @@ sed -i 's/192.168.1.1/192.168.8.2/g' package/base-files/files/bin/config_generat
 git clone https://github.com/jefferymvp/luci-app-koolproxyR package/lean/luci-app-koolproxyR
 git clone https://github.com/tty228/luci-app-serverchan package/lean/luci-app-serverchan
 git clone https://github.com/frainzy1477/luci-app-clash package/lean/luci-app-clash
-git clone https://github.com/vernesong/OpenClash package/lean/OpenClash
+git clone https://github.com/vernesong/OpenClash package/lean/luci-app-OpenClash
 ./scripts/feeds update -a
 ./scripts/feeds install -a
